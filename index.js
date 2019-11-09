a9b8815c96aace60e899be8c63ef3d7ff85286b7
AJ Guiles <ashiguiles@gmail.com>
2019-11-09 05:38:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ let createEmployees = function(employeeRowData) {
     })
 }
 
+// Loads Array elements into corresponding Object properties. Additionally, initialize empty Arrays on the properties timeInEvents and timeOutEvents.
+
 let createTimeInEvent = function(dateStamp){
     let [date, hour] = dateStamp.split(' ')
 
