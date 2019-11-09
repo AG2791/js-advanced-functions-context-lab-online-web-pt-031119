0ef10de93d0c8165f243301a16bd14eeb580944e
AJ Guiles <ashiguiles@gmail.com>
2019-11-09 05:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ let allWagesFor = function () {
     return payable
 }
 
+Using wagesEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context. Amount should be returned as a number.
+
 let createEmployeeRecord = function(row){
     return {
         firstName: row[0],
